9cb2349e5e45d8c895a6fafd95342b7597d73f9e
2020j <2020j@LAPTOP-DIO2AA9S.unl.edu>
2021-08-28 16:02:05 -0500

    Added name and major

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		System.out.println("Jack Steckler!");
+		System.out.println("Computer Science");
 	}
 }
